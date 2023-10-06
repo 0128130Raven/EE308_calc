508579bb4a2f819551a85a8426343e78244e8034
SQ <466542499@qq.com>
2023-10-07 07:42:44 +0800

    上传

---
+++ calc_simple_test/calc.js
@@ -0,0 +1,36 @@
+var IsClear = false;
+var cal = "";
+function get(key) {
+  var str = document.getElementById("text").value;
+  if (IsClear) {
+    str = "0";
+    IsClear = false;
+  }
+  if (str.length < 20) {
+    str = str == "0" ? "" : str;
+    if (str == "" && key == "00") {
+      str = "0";
+    } else {
+      str += key;
+    }
+  }
+  document.getElementById("text").value = str;
+}
+function goBack() {
+  var str = document.getElementById("text").value;
+  str = str.substring(0, str.length - 1);
+  if (str == "") str = "0";
+  document.getElementById("text").value = str;
+}
+function clearText() {
+  document.getElementById("text").value = "0";
+}
+function eq() {
+  IsClear = true;
+  var str = document.getElementById("text").value;
+  var result = eval(str);
+  if (result == "Infinity") {
+    result = "输入有误";
+  }
+  document.getElementById("text").value = result;
+}
